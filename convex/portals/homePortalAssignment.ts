f882e275aa216445fb1690508aaa02ccf62ccef9
Connorbelez <connor.belez@gmail.com>
2026-04-22 12:33:57 -0400

    fix(portals): remove duplicate home portal import

--- convex/portals/homePortalAssignment.ts
+++ convex/portals/homePortalAssignment.ts
@@ -1,6 +1,5 @@
 import type { DataModel, Doc, Id } from "../_generated/dataModel";
 import type { MutationCtx, QueryCtx } from "../_generated/server";
-import { getDeterministicPortalIdForOrgId } from "./borrowerPortalAttribution";
 import {
 	deleteOrphanUsersByAuthId,
 	findCanonicalUserByAuthId,
